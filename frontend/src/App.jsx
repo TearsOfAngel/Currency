ee339dc0bcfcb13128c7245ca5653a3e8ffccf34
a.suchkova <suchkovalika065@gmail.com>
2024-01-18 13:25:36 +0400

    init front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import UploadService from './services/UploadService.jsx'
+
+function App() {
+	return (
+		<>
+			<UploadService />
+		</>
+	)
+}
+
+export default App
---
+++ frontend/src/services/UploadService.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+import axios from 'axios'
+
+function UploadService() {
+	const [file, setFile] = useState(null)
+
+	const onFileChange = event => {
+		setFile(event.target.files[0])
+	}
+
+	const onSubmit = event => {
+		event.preventDefault()
+		const formData = new FormData()
+		formData.append('file', file)
+
+		axios
+			.post('http://localhost:8080/api/upload', formData)
+			.then(response => {
+				console.log('File uploaded successfully', response.data)
+			})
+			.catch(error => {
+				console.error('There was an erro uploading the file', error)
+			})
+	}
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input type='file' onChange={onFileChange} />
+				<button>Upload!</button>
+			</form>
+		</div>
+	)
+}
+
+export default UploadService
